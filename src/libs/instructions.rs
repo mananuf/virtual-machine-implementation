836b471d6dfbbaec815425f64739fe62b72a767a
mananuf <bmananaf@gmail.com>
2025-09-08 16:16:02 +0100

    chore: optimizes fn and

--- src/libs/instructions.rs
+++ src/libs/instructions.rs
@@ -51,12 +51,15 @@ impl InstructionSet for Instructions {
     ) -> Result<(), InstructionSetError> {
         let result = register_storage.locations[destination_register as usize];
         if result == 0 {
-            let _set_zero_conditional_flag= register_storage.store(ConditionalFlags::ZRO as u16, Registers::COND);
+            let _set_zero_conditional_flag =
+                register_storage.store(ConditionalFlags::ZRO as u16, Registers::COND);
         } else if result >> 15 == 1 {
             /* a 1 in the left-most bit indicates negative */
-            let _set_negative_condition = register_storage.store(ConditionalFlags::NEG as u16, Registers::COND);
+            let _set_negative_condition =
+                register_storage.store(ConditionalFlags::NEG as u16, Registers::COND);
         } else {
-            let _set_positive_condition = register_storage.store(ConditionalFlags::POS as u16, Registers::COND);
+            let _set_positive_condition =
+                register_storage.store(ConditionalFlags::POS as u16, Registers::COND);
         }
         Ok(())
     }
@@ -99,23 +102,28 @@ impl InstructionSet for Instructions {
 
     fn and(register_storage: &mut RegisterStorage, instr: u16) -> Result<(), InstructionSetError> {
         /* destination register (DR) */
-        register_storage.locations[Registers::R0 as usize] = (instr >> 9) & 0x7;
+        let _set_destination_reg = register_storage.store((instr >> 9) & 0x7, Registers::R0);
 
         /* first operand (SR1) */
-        register_storage.locations[Registers::R1 as usize] = (instr >> 6) & 0x7;
+        let _set_source_register_1 = register_storage.store((instr >> 6) & 0x7, Registers::R1);
 
         /* whether we are in immediate mode */
         let imm_flag = (instr >> 5) & 0x1;
 
         if imm_flag == 1 {
             let imm5 = Self::sign_extend(instr & 0x1F, 5).unwrap();
-            register_storage.locations[Registers::R0 as usize] =
-                register_storage.locations[Registers::R1 as usize] & imm5;
+            let _update_destination_reg_with_imm5 = register_storage.store(
+                register_storage.load(Registers::R1).unwrap() & imm5,
+                Registers::R0,
+            );
         } else {
-            register_storage.locations[Registers::R2 as usize] = instr & 0x7;
-            register_storage.locations[Registers::R0 as usize] = register_storage.locations
-                [Registers::R1 as usize]
-                & register_storage.locations[Registers::R2 as usize];
+            let _set_source_reg_2 = register_storage.store(instr & 0x7, Registers::R2);
+
+            let _update_destination_reg_without_imm5 = register_storage.store(
+                register_storage.load(Registers::R1).unwrap()
+                    & register_storage.load(Registers::R2).unwrap(),
+                Registers::R0,
+            );
         }
 
         let _ = Self::update_flags(register_storage, Registers::R0);
