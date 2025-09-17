e1710e291cde0e16cced00d60b40f47bfdc0afc6
mananuf <bmananaf@gmail.com>
2025-09-17 02:29:09 +0100

    WIP

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,4 +1,4 @@
-use std::{process::abort, u16};
+use std::{io::{self, Read}, process::abort, u16};
 
 use tracing::info;
 use virtual_machine::libs::{
@@ -19,11 +19,11 @@ fn main() {
 
     // since exactly one condition flag should be set at any given time, set the Z flag
     let _set_initial_cond_to_zero =
-        register_storage.store(ConditionalFlags::ZRO as u16, Registers::COND);
+        register_storage.store(ConditionalFlags::ZRO as u16, Registers::COND as u16);
 
     /* set the PC to starting position */
     /* 0x3000 is the default */
-    let _set_default_mem_addr_in_pc = register_storage.store(PC_START, Registers::PC);
+    let _set_default_mem_addr_in_pc = register_storage.store(PC_START, Registers::PC as u16);
 
     // loop {
     /* FETCH: Get the instruction from memory at the address pointed to by the PC */
@@ -102,15 +102,17 @@ fn main() {
     // let r1 = (instr >> 6)  & 0x7;
     // let imm_flag = (instr >> 5)  & 0x1;
     // println!("{instr :#01x} {r0} {r1} {imm_flag}");
-    let bit_count: u32 = 5;
-    let bits = 0b01011;
-    let sign_extend_result = Instructions::sign_extend(bits, bit_count).unwrap();
+    // let bit_count: u32 = 5;
+    // let bits = 0b01011;
+    // let sign_extend_result = Instructions::sign_extend(bits, bit_count).unwrap();
 
-    println!("{:016b}", sign_extend_result);
-    println!("{:04x}", 0b0000111000000000);
-    println!("{:04x}", 0b0000000111111111);
-    println!("{:04x}", 0b0000000000000111);
+    // println!("{:016b}", sign_extend_result);
+    // println!("{:04x}", 0b0000111000000000);
+    // println!("{:04x}", 0b0000000111111111);
+    // println!("{:04x}", 0b0000000000000111);
 
-    println!("{:b}", sign_extend_result);
+    // println!("{:b}", sign_extend_result);
+
+    
     println!("{register_storage:?}")
 }
